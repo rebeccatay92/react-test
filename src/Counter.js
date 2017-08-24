395a933cb2dca94162d5f60c9dd1600cf7e4d739
Rebecca Tay <rebeccatay92@gmail.com>
2017-08-24 15:43:02 +0800

    filter names by keyword

--- src/Counter.js
+++ src/Counter.js
@@ -9,7 +9,7 @@ class Counter extends Component {
     this.state = {
       number: props.num,
       keyword: 'Search keyword',
-      enemiesArr: ['The Hound', 'Cersei', 'Sansa']
+      enemiesArr: ['The Hound', 'The Cersei', 'The Sansa']
     }
   }
 
@@ -33,10 +33,23 @@ class Counter extends Component {
     this.setState(e.target.value ? { keyword: e.target.value } : { keyword: 'Search keyword' })
   }
 
+  filterName (e) {
+    let keyword = e.target.value
+
+    const result = this.state.enemiesArr.filter((name) => {
+      console.log(name, keyword)
+      return name.toLowerCase().includes(keyword.toLowerCase())
+    })
+    console.log(result)
+    // this.setState({
+    //   enemiesArr: this.state.enemiesArr.concat(result)
+    // })
+  }
+
   updateList (e) {
     console.log(e)
     this.setState({
-      namesArr: this.state.namesArr.concat(this.state.name)
+      enemiesArr: this.state.enemiesArr.concat(this.state.name)
     })
     //dont use push. push is mutative and props/states/react must be immutable
   }
@@ -53,7 +66,7 @@ class Counter extends Component {
         <div>
           <label>
             Type your name:
-            <input type='text' onChange={(e) => this.showName(e)} />
+            <input type='text' onChange={(e) => this.filterName(e)} />
           </label>
           <button onClick={(e) => this.updateList(e)}>Add name</button>
         </div>
@@ -67,21 +80,26 @@ class Counter extends Component {
   }
 
   // only run the api after components have been initialized / rendered
-  componentDidMount () {
-    const url = `https://api.got.show/api/characters`
-
-    fetch(url)
-      .then((response) => {
-        console.log(response)
-        return response.json()
-      }) // processes the promise object into json
-      .then((data) => {
-        console.log('data', data)
-      }) // reads the process json
-      .catch((err) => {
-        console.log(err)
-      }) // catch error
-  }
+//   componentDidMount () {
+//     const url = `https://api.got.show/api/characters`
+//
+//     fetch(url)
+//       .then((response) => {
+//         console.log(response)
+//         return response.json()
+//       }) // processes the promise object into json
+//       .then((data) => {
+//         console.log('data', data)
+//         data.map((character) => {
+//           this.setState({
+//             enemiesArr: this.state.enemiesArr.concat(character.name)
+//           })
+//         })
+//       }) // reads the process json
+//       .catch((err) => {
+//         console.log('err', err)
+//       }) // catch error
+//   }
 }
 
 export default Counter
